e591cc4b1df0b04821e930efabb67bcc1bcf7a9e
Rahul keshri <rkeshri2021@gmail.com>
2023-09-12 01:56:58 +0530

    Type alias in typescript

--- Types/object.ts
+++ Types/object.ts
@@ -1,5 +1,20 @@
+//without type
+
 const printUserDetails = (user: {name: string, age: number}): string => {
     return `Hi, ${user.name}. You are ${user.age} years old!`;
 }
 
 console.log(printUserDetails({name: 'RK', age: 23}))
+
+// type alias
+
+type PerfectUser = {
+    name: string,
+    age: number
+}
+
+const printUserDetailsTwo = (user: PerfectUser): string => {
+    return `Hi, ${user.name}. You are ${user.age} years old!`;
+}
+
+console.log(printUserDetailsTwo({name: 'RK', age: 25}))
