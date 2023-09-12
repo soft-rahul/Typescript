2437bc60358e7698d3257719ee62abfe9e85634f
Rahul keshri <rkeshri2021@gmail.com>
2023-09-13 01:18:02 +0530

    Day 2 of Learning Typescript

--- Types/union.ts
+++ Types/union.ts
@@ -4,9 +4,19 @@ x = '100';
 
 // array 
 const ages: (number | string | boolean)[] = [1,2,3,4,'one','two', true];
+const stuff: number[] | string[] = ['a','b'];
 
 // literal with union
 let gender: 'male' | 'female' | 'other';
 
 gender = "female";
 gender = 'male';
+
+// Exercise : 1
+type SkillLevel  =  "Beginner" | 'Intermediate' | 'Advanced' | 'Expert';
+type SkiSchoolStudent = {
+    name: string;
+    age: number;
+    sport: 'ski' | 'snowboard';
+    level: SkillLevel;
+}
