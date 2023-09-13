5c6b30d29a76f3ca58d5048ec0a4e760ef8cfc7f
Rahul keshri <rkeshri2021@gmail.com>
2023-09-13 23:46:55 +0530

    Type for function return type

--- Types/function.ts
+++ Types/function.ts
@@ -1,11 +1,10 @@
-function sum(num1: number, num2: number) {
-    return num1 + num2;
+function sum(num1: number, num2: number): number {
+  return num1 + num2;
 }
 
-const arrowSum = (num1: number, num2: number) => num1 + num2;
-
-const greetUser = (name: string = 'User') => `Hi, ${name}`;
-const greetUser2 = (name: string) => `Hi, ${name}`;
+const arrowSum = (num1: number, num2: number): number => num1 + num2;
+const greetUser = (name: string = "User"): string => `Hi, ${name}`;
+const greetUser2 = (name: string): string => `Hi, ${name}`;
 
 greetUser();
 greetUser2("New User");
